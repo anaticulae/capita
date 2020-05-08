8c47cf99aa2b0bade374b8aff6320e013d71b376
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-05-08 14:44:12 +0200

    0.2.19 automatically generated release

--- sections/__init__.py
+++ sections/__init__.py
@@ -68,7 +68,7 @@ The following sections are planned to be supported:
 
 import os
 
-__version__ = '0.2.18'
+__version__ = '0.2.19'
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
