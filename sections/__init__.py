758b7e8ccb79c10aa1c08a4c3f97845d48017957
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-05-18 15:33:36 +0200

    0.2.24 automatically generated release

--- sections/__init__.py
+++ sections/__init__.py
@@ -68,7 +68,7 @@ The following sections are planned to be supported:
 
 import os
 
-__version__ = '0.2.23'
+__version__ = '0.2.24'
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
