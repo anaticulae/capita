5f8d255e7013a2161d06276bfd1dab00fbbf3014
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-06-13 21:21:18 +0200

    refactor(replace): replace with german code

--- sections/feature/bibliography.py
+++ sections/feature/bibliography.py
@@ -7,14 +7,9 @@
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
 
-import operator
-# TODO: MOVE TO TEXMEX
-import re
-
 import german
 import serializeraw
 import texmex
-import utila
 
 import sections.utils.spa
 
@@ -69,7 +64,7 @@ def analyse_page(navigator: texmex.PageTextNavigator
                 ) -> sections.feature.StatisticalResultItem:
     raw = ' '.join([line.text for line in navigator])
     collected = []
-    for method in [years, dates, pagenumbers]:
+    for method in [german.years, german.dates, german.pagenumbers]:
         collected.extend(method(raw))
 
     marker = len(collected)
@@ -98,70 +93,3 @@ def special_chars(raw: str) -> list:
     if word_count > 40 and classifier > 0.3:  # TODO HOLY VALUE
         return True
     return False
-
-
-def years(raw: str, min_=1950, max_=2020):
-    """Extract sorted list of years out of `raw` text.
-
-    >>> years('1999, Helm is born in 1987. Mud exists since 1800. 2050 20000 2020')
-    [1987, 1999, 2020]
-    """
-    result = []
-    pattern = r'\b(19|20)\d{2}\b'
-    for item in re.finditer(pattern, raw):
-        item = utila.extract_match(item)
-        year = int(item)
-        if min_ <= year <= max_:
-            result.append(year)
-    result = sorted(result)
-    return result
-
-
-def dates(raw: str, min_year=1950, max_year=2020):
-    """Extract sorted list of dates out of `raw` text.
-
-    >>> dates('Stand 20.10.2020, (15.11.2014), 01.01.1999 01.01.1940')
-    [(1999, 1, 1), (2014, 11, 15), (2020, 10, 20)]
-    """
-    result = []
-    pattern = r'(\d{1,2})\.(\d{1,2})\.(\d{4})'
-    for item in re.findall(pattern, raw):
-        day, month, year = item
-        day, month, year = int(day), int(month), int(year)
-        if not 1 <= day <= 31:
-            continue
-        if not 1 <= month <= 12:
-            continue
-        if not min_year <= year <= max_year:
-            continue
-        result.append((year, month, day))
-    result = sorted(result, key=operator.itemgetter(0, 1, 2))
-    return result
-
-
-def pagenumbers(raw: str):
-    """Extract single pages and page ranges out of `raw` text.
-
-    >>> pagenumbers('S. 13-50 S.30 S. 1-5 S.319-350, Seite 20-30., page 500 p.4')
-    [(1, 5), (4, 4), (13, 50), (20, 30), (30, 30), (319, 350), (500, 500)]
-    """
-    result = []
-    pattern = r"""(S|S\.|Seite|p|p\.|page)
-                [ ]{0,4}
-                (
-                    (?P<pstart>\d{1,4})[ ]{0,2}\-[ ]{0,2}(?P<pend>\d{1,4})|
-                    (?P<page>\d{1,4})
-                )
-               """
-    for item in re.finditer(pattern, raw, re.VERBOSE):
-        try:
-            # single page
-            pstart = int(item['page'])
-            result.append((pstart, pstart))
-        except TypeError:
-            # from page start till page end
-            pstart = int(item['pstart'])
-            pend = int(item['pend'])
-            result.append((pstart, pend))
-    result = sorted(result, key=operator.itemgetter(0))
-    return result
