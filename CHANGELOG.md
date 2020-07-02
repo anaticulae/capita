44ba0e8cbaa5545399d8dc64da000992b0e2a8df
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-07-02 19:06:31 +0200

    0.4.10 automatically generated release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 
 Every noteable change is logged here.
 
+## v0.4.10
+
 ## v0.4.9
 
 ## v0.4.8
--- sections/__init__.py
+++ sections/__init__.py
@@ -68,7 +68,7 @@ The following sections are planned to be supported:
 
 import os
 
-__version__ = '0.4.9'
+__version__ = '0.4.10'
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
