284bf842782cf04d676298666f40f9702f23968a
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-07-27 18:35:36 +0200

    chore(requirements): upgrade requirements.txt
    + adjust to new power API

--- tests/resources/update.py
+++ tests/resources/update.py
@@ -15,19 +15,19 @@ import sections
 power.setup(sections.ROOT)
 
 RESOURCES = [
-    power.todo(power.MASTER072_PDF),
-    power.todo(power.MASTER098_PDF),
-    power.todo(power.BACHELOR037_PDF, '0:30'),
-    power.todo(power.BACHELOR063_PDF, '0:20,59,60,61'),
-    power.todo(power.BACHELOR111_PDF, '0:10,90:111'),
-    power.todo(power.BACHELOR056_PDF, '0:55'),
-    power.todo(power.MASTER116_PDF, '0:13,85:117'),
-    power.todo(power.BACHELOR090_PDF, '0:20,75:90'),
-    power.todo(power.DOCU14_PDF),
-    power.todo(power.DOCU07_PDF),
-    power.todo(power.DOCU09_PDF),
-    power.todo(power.DOCU27_PDF),
-    power.todo(power.DOCU35_PDF),
+    (power.MASTER072_PDF, None),
+    (power.MASTER098_PDF, None),
+    (power.BACHELOR037_PDF, '0:30'),
+    (power.BACHELOR063_PDF, '0:20,59,60,61'),
+    (power.BACHELOR111_PDF, '0:10,90:111'),
+    (power.BACHELOR056_PDF, '0:55'),
+    (power.MASTER116_PDF, '0:13,85:117'),
+    (power.BACHELOR090_PDF, '0:20,75:90'),
+    (power.DOCU14_PDF, None),
+    (power.DOCU07_PDF, None),
+    (power.DOCU09_PDF, None),
+    (power.DOCU27_PDF, None),
+    (power.DOCU35_PDF, None),
 ]
 
 REQURIED_RESOURCES = [power.link(pdf) for pdf, _ in RESOURCES]
