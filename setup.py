54e9f310f89565dec996d9b7f9d7f667c2a86090
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-08-10 21:01:26 +0200

    fix(setup): add missing package

--- setup.py
+++ setup.py
@@ -48,6 +48,7 @@ if __name__ == "__main__":
             'sections',
             'sections.biblio',
             'sections.feature',
+            'sections.geometry',
             'sections.table',
             'sections.utils',
             'sections.workplan',
