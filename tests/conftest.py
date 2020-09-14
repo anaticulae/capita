eaf76b60506eb3047e88f9fdafdeb7b69a2086d8
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-09-14 21:22:49 +0200

    chore(test): simplify test data generator

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,21 +12,36 @@ import power
 import pytest
 
 import sections
-import tests.resources.update
 
 pytest_plugins = ['pytester', 'xdist']  # pylint: disable=invalid-name
 
+power.setup(sections.ROOT)
+
 PACKAGE = sections.PACKAGE
-RESOURCES = tests.resources.update.RESOURCES
 
 WORKER = 6
 
-power.setup(sections.ROOT)
+RESOURCES = [
+    (power.MASTER072_PDF, None),
+    (power.MASTER098_PDF, None),
+    (power.BACHELOR037_PDF, None),
+    (power.DISS264_PDF, '0:50'),
+    (power.BACHELOR063_PDF, '0:20,59,60,61'),
+    (power.BACHELOR111_PDF, '0:10,90:111'),
+    (power.BACHELOR056_PDF, '0:55'),
+    (power.MASTER116_PDF, '0:13,85:117'),
+    (power.BACHELOR090_PDF, '0:20,75:90'),
+    (power.DOCU14_PDF, None),
+    (power.DOCU07_PDF, None),
+    (power.DOCU09_PDF, None),
+    (power.DOCU27_PDF, None),
+    (power.DOCU35_PDF, None),
+]
 
 
 @pytest.mark.usefixtures('session')
 def pytest_sessionstart():
-    power.run(tests.resources.update.REQURIED_RESOURCES)
+    power.run()
 
 
 def extract(resources):
--- tests/validation/test_validation.py
+++ tests/validation/test_validation.py
@@ -14,7 +14,6 @@ import utila
 import utilatest
 
 import sections.feature.section
-import tests.resources.update
 import tests.validation.bachelor
 import tests.validation.master
 
