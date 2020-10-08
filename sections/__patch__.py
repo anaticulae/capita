1c1f0504d7040e5664e3ebbce8dd1c68c28b2ee3
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-10-08 10:12:06 +0200

    feat(patch): add authors to extract list of authors

--- sections/__patch__.py
+++ sections/__patch__.py
@@ -6,3 +6,30 @@
 # use or distribution is an offensive act against international law and may
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
+
+import re
+
+import german
+import utila
+
+
+def authors(raw: str) -> list:
+    """\
+    >>> authors('PEREIRA, M.G., VOLCHAN, E., SOUZA, G. G. DE, OLIVEIRA, L.,'
+    ... 'CAMPAGNOLI, R. R., PINHEIRO, W. M., & PESSOA, L. ')
+    ['PEREIRA, M.G.', 'VOLCHAN, E.', 'SOUZA, G. G. DE', 'OLIVEIRA, L.', 'CAMPAGNOLI, R. R.', 'PINHEIRO, W. M.', 'PESSOA, L.']
+    """
+    pattern = r"""
+        [a-zA-Z]{4,}\,
+        [ ]{0,2}
+        ([a-zA-Z]\.[ ]{0,2}){1,3}
+        [ ]{0,2}
+        (DE)?
+    """
+    result = []
+    for item in re.finditer(pattern, raw, re.VERBOSE):
+        result.append(utila.extract_match(item).strip())
+    return result
+
+
+german.authors = authors
