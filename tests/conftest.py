860250c05dab0a646079112f6ea9fa5b81a7f07b
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-01-04 19:53:30 +0100

    chore(resources): extend test data generator

--- tests/conftest.py
+++ tests/conftest.py
@@ -22,6 +22,7 @@ PACKAGE = sections.PACKAGE
 WORKER = 6
 
 RESOURCES = [
+    (power.DISS266_PDF, None),
     (power.MASTER110_PDF, None),
     (power.MASTER072_PDF, None),
     (power.MASTER098_PDF, None),
