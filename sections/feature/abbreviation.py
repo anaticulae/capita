c082c4772fb7402cf749105546e02a1489d1c667
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-02-05 01:16:19 +0100

    feat(abbreviation): use better headline selector strategy

--- sections/feature/abbreviation.py
+++ sections/feature/abbreviation.py
@@ -9,65 +9,62 @@
 """Abbreviation Likelihood Detection
 =================================
 
-Currently there are two different approaches to differentiate between
-abbreviation and non abbreviation pages. On the first side we check that
-there are not to much sentences on a page. On the other side we search
-for selective words "Abkuerzungsverzeichnis, ...".
+We search for selective headline "Abkuerzungsverzeichnis, ...".
 
 NOTE: This approach is only for demo time.
 """
 
+import iamraw
 import serializeraw
-import texmex
 
 import sections.feature
-import sections.utils.spa
-import sections.utils.text
+import sections.utils.headline
 
-TITLE = ['Abkürzungsverzeichnis']
+NO_PAGE = (0, 0)
 
 
-def work(document: str, position: str, pages=None) -> str:
-    data = sections.utils.spa.Data(
-        document=document,
-        position=position,
+def work(oneline_text: str, oneline_textpositions: str, pages=None) -> str:
+    navigators = serializeraw.create_pagetextnavigators_fromfile(
+        oneline_text,
+        oneline_textpositions,
         pages=pages,
     )
 
-    config = sections.utils.spa.Config(
-        likelihood_name='abbreviation_table',
-        page_analysis=analyse_page,
-    )
+    result = {page.page: analyse_page(page) for page in navigators}
 
-    extracted = sections.utils.spa.work(
-        data=data,
-        config=config,
-    )
+    uniformed = sections.feature.uniform_result(result)
+    multiformed = sections.feature.multiform_result(result)
+    if multiformed is not None:
+        uniformed = multiformed
+    assert len(uniformed) == len(navigators)
 
-    dumped = serializeraw.dump_likelihood(extracted)
+    result = [
+        iamraw.PageContentLikelihood(
+            page=page,
+            content=iamraw.Likelihood(value, name='abbreviation_table'),
+        ) for page, value in uniformed.items()
+    ]
+    result = sorted(result, key=lambda x: x.page)
+
+    dumped = serializeraw.dump_likelihood(result)
     return dumped
 
 
-def analyse_page(navigator: texmex.PageTextNavigator
-                ) -> sections.feature.StatisticalResultItem:
-    result = []
-    if not is_abbreviation_table_statistical(navigator):
-        return (0, 0.0)
-
-    for line in navigator:
-        line = line.text.strip()
-        if line in TITLE:
-            result.append(line)
-
-    if not result:
-        # could not find any TITLE line
-        return (0, 0)
-    return (len(result), 1.0)
+HEADLINES = [
+    'Abbreviations',
+    'Abbreviationtable',
+    'Abkürzungen',
+    'Abkürzungsverzeichnis',
+]
 
 
-def is_abbreviation_table_statistical(navigator: texmex.PageTextNavigator):
-    textonpage = sections.utils.text.textonpage(navigator)
-    if len(textonpage.sentences) > 5:
-        # TODO: HOLY VALUE
-        return False
-    return True
+def analyse_page(content):
+    headlines = sections.utils.headline.headlines(content)
+    if not headlines:
+        return NO_PAGE
+    if isinstance(headlines, str):
+        headlines = [headlines]
+    for item in HEADLINES:
+        if item in headlines:
+            return 1, 1
+    return NO_PAGE
