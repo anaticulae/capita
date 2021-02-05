cfc64c5474beb0cfc1f059342ec07a3f1beaa596
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-02-05 02:34:46 +0100

    chore(requirements): upgrade requirements.txt

--- tests/conftest.py
+++ tests/conftest.py
@@ -53,13 +53,6 @@ def pytest_sessionstart():
 
 
 def extract(resources):
-    # ensure to handle single file generation or common resource subfolder
-    # correctly. To determine the output path it is required to determine
-    # the parent path of at least two files. If resources provide only a
-    # single file the parental determination is not possible. Therefore we
-    # have to add the data root of all test files.
-    resources.append(power.REPOSITORY)
-
     genex.extract(
         files=resources,
         destination=power.generated(),
