86b6e1325eb9007e1caf0eb2cb3d01c6da5eae6b
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-05-27 18:58:40 +0200

    fix(chapter): make approach more stable against caps

--- sections/feature/chapter.py
+++ sections/feature/chapter.py
@@ -178,7 +178,7 @@ def contain_chapter(content) -> float:  # pylint:disable=R1260
             matched = re.match(NUMBER_PATTERN, line)
             if not matched:
                 continue
-            if any(item in line for item in HEADLINES_CHAPTER):
+            if any(item in line.lower() for item in HEADLINES_CHAPTER):
                 return True
         return False
 
@@ -215,12 +215,11 @@ Vorwort
 Zusammenfassung
 """)
 
-# TODO: Make more robust against CAPS
-HEADLINES_CHAPTER = {
-    'Einleitung',
-    'Introduction',
-    'Grundlagen',
-}
+HEADLINES_CHAPTER = utila.splitlines("""
+Einleitung
+Introduction
+Grundlagen
+""")
 
 
 def contain_toc(content, toc) -> float:
