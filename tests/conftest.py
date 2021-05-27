8ec9dd94ee2b06c40469d3fb2d03d250f131bc32
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-05-27 19:46:02 +0200

    refactor(test): rename to new API

--- tests/conftest.py
+++ tests/conftest.py
@@ -30,7 +30,7 @@ RESOURCES = [
     (power.BACHELOR128_PDF, None),
     (power.BACHELOR037_PDF, None),
     (power.DOCU35_PDF, None),
-    (power.ORDER109_PDF, None),  # TODO: CHANGE TO 107
+    (power.ORDER107_PDF, None),
     (power.MASTER091A_PDF, None),
     (power.MASTER031_PDF, None),
     (power.MASTER075_PDF, None),
