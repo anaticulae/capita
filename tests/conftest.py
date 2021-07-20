860aa9a9363a8608301ce8c546c8a3365ef4b83c
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-07-20 18:54:50 +0200

    test(fixture): add paper18 navigator fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,6 +13,7 @@ import pytest
 
 import sections
 # pylint:disable=W0611
+from tests.fixtures.paper import paper18
 from tests.fixtures.restruct import restructured_fontstore
 from tests.fixtures.restruct import restructured_fontstore_fixture
 from tests.fixtures.restruct import restructured_sections_manual
---
+++ tests/fixtures/paper.py
@@ -0,0 +1,19 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2021 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import power
+import pytest
+import serializeraw
+
+
+@pytest.fixture
+def paper18():
+    source = power.link(power.PAPER18_PDF)
+    navigator = serializeraw.create_pagetextnavigators_frompath(source)
+    return navigator
