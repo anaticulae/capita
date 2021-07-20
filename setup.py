660e9ae4a8c395ab133327870273ee1ffa7701a6
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-07-20 18:06:29 +0200

    feat(paper): add paper detection package

--- setup.py
+++ setup.py
@@ -48,6 +48,7 @@ if __name__ == "__main__":
             'sections',
             'sections.biblio',
             'sections.feature',
+            'sections.paper',
             'sections.table',
             'sections.utils',
         ],
