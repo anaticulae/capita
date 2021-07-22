11f67c30a77fad489928640ca00d51ed13525f46
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-07-22 13:45:30 +0200

    feat(paper): add page font detection

---
+++ sections/paper/fonts.py
@@ -0,0 +1,32 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2021 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import collections
+
+import texmex
+
+
+def bypage(navigators: texmex.PageTextNavigators):
+    result = []
+    for page in navigators:
+        parsed = parse_page(page)
+        result.append(parsed)
+    return result
+
+
+def parse_page(navigator: texmex.PageTextNavigator):
+    fontsize = collections.defaultdict(int)
+    fontface = collections.defaultdict(int)
+    for line in navigator:
+        for style in line.style.content:
+            width = (style.end - style.start)
+            fontsize[style.size] += width
+            fontface[style.font] += width
+    fontsize, fontface = dict(fontsize), dict(fontface)
+    return fontsize, fontface
---
+++ tests/paper/test_fonts.py
@@ -0,0 +1,16 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2021 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import sections.paper.fonts
+
+
+def test_fonts_bypage(paper18):
+    # this test is not very usefull yet. Fonts where not used in the moment.
+    parsed = sections.paper.fonts.bypage(paper18)
+    assert parsed
