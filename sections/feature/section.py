eb345d48637e94c1f7106bdf2bcf7a037b336875
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-01-01 04:58:15 +0100

    fix(section): skip multiple sections for abbr and symbol table

--- sections/feature/section.py
+++ sections/feature/section.py
@@ -153,6 +153,14 @@ def extract_sections(loaded: SectionsRequiredResources) -> iamraw.Sections:
 
 def morethan_one(trusted) -> bool:
     if len(trusted) > 1:
+        if len(trusted) == 2:
+            names = [item.content.name for item in trusted]
+            # in the current state, we are not able to distinguish between
+            # symbol table and abbr table. This is not necessary for our
+            # job here.
+            if 'abbreviation_table' in names and 'symboltable' in names:
+                # TODO: REMOVE LATER
+                return False
         return True
     return False
 
