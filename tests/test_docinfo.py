8d9abe08fe2cc68c68542f0a0b554f26a9436995
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-01-13 18:18:50 +0100

    test(docinfo): add test to verify language detection

---
+++ tests/test_docinfo.py
@@ -0,0 +1,25 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2022 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import iamraw
+import power
+import serializeraw
+import utilatest
+
+import tests
+
+
+def test_docinfo_lang(testdir, monkeypatch):
+    pdf = power.DISS406_PDF
+    utilatest.fixture_requires(pdf)
+    source = power.link(pdf)
+    command = f'-i {source} -o {testdir.tmpdir} --pages=0:10'
+    tests.run_sections(command, monkeypatch=monkeypatch)
+    docinfo = serializeraw.load_docinfo(testdir.tmpdir)
+    assert docinfo.lang == iamraw.Language.GERMAN
