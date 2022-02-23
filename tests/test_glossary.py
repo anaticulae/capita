9a833a86ae2d030d338e5333109cd51527c54750
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-02-23 17:50:13 +0100

    test(glossary): add regression test

--- tests/test_glossary.py
+++ tests/test_glossary.py
@@ -26,6 +26,7 @@ def test_glossary_work_diss143():
 @utilatest.nightly
 @pytest.mark.parametrize('source, pages', [
     pytest.param(power.power.BOOK173_PDF, None, id='book173'),
+    pytest.param(power.power.DISS173_PDF, '0:50', id='diss173'),
 ])
 def test_no_glossary_regression_x(source, pages):
     """Do not detect page 142 as glossary. It's just a page about glossaries."""
