5ddb3afb58c0681d642cde763a2fbb370f910c41
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-02-23 20:16:53 +0100

    test(fix): adjust file name

--- tests/test_glossary.py
+++ tests/test_glossary.py
@@ -26,9 +26,9 @@ def test_glossary_work_diss143():
 
 @utilatest.nightly
 @pytest.mark.parametrize('source, pages', [
-    pytest.param(power.power.BOOK173_PDF, None, id='book173'),
-    pytest.param(power.power.DISS173_PDF, utila.rtuple(50), id='diss173'),
-    pytest.param(power.power.BACHELOR076_PDF, None, id='bachelor076'),
+    pytest.param(power.BACHELOR076_PDF, None, id='bachelor076'),
+    pytest.param(power.BOOK173_PDF, None, id='book173'),
+    pytest.param(power.DISS173_PDF, utila.rtuple(50), id='diss173'),
 ])
 def test_no_glossary_regression_x(source, pages):
     """Do not detect page 142 as glossary. It's just a page about glossaries."""
