888efad3cac233ba91d1c397147fb4a2d02b5794
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-04-03 16:46:19 +0200

    test(validate): validate sections

---
+++ tests/test_validate.py
@@ -0,0 +1,72 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2022 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import functools
+
+import power
+import pytest
+import serializeraw
+import utila
+import utilatest
+
+import sections
+import tests
+
+ARCHIVE = utila.join(sections.ROOT, 'tests/expected', exist=True)
+
+step = lambda x: pytest.param(x, ':', utila.file_name(x), id=utila.file_name(x))
+
+
+@pytest.mark.parametrize('source, pages, expected', [
+    step(power.DOCU009_PDF),
+])
+@utilatest.nightly
+def test_sections_validate(source, pages, expected, testdir, monkeypatch):
+    utilatest.fixture_requires(source)
+    Evaluate(
+        source=source,
+        pages=pages,
+        expected=expected,
+        workdir=testdir.tmpdir,
+        monkeypatch=monkeypatch,
+    ).evaluate()
+
+
+class Evaluate(utilatest.BaseLiner):
+
+    def __init__(self, source, pages, expected, workdir, monkeypatch):
+        super().__init__(
+            # step=f'pdf {source}',
+            step='',
+            program=functools.partial(
+                tests.run_sections,
+                monkeypatch=monkeypatch,
+            ),
+            pages=pages,
+            source=power.link(source),
+            workdir=workdir,
+            archive=ARCHIVE,
+            loader=self.load_sections,
+            convert_source=False,
+            index=expected,
+        )
+
+    def load_sections(self, _):  # pylint:disable=W0613
+        loaded = serializeraw.load_sections(self.workdir)
+        return loaded
+
+    def raw(self, value) -> str:
+        result = []
+        for item in value:
+            start = str(item.start).zfill(3)
+            end = str(item.end).zfill(3)
+            line = f'{start} {end} {item.__class__.__name__}'
+            result.append(line)
+        raw = utila.NEWLINE.join(result)
+        return raw
