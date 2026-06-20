34e86f9dd3432dbb3e6309a2d7bceb16c223a3af
Helmut Konrad Schewe <helmutus@outlook.com>
2026-06-20 13:19:45 +0200

    feat(patch): add method to generate unions

--- sections/__patch__.py
+++ sections/__patch__.py
@@ -36,3 +36,35 @@ def authors(raw: str, verbose: bool = False) -> list:
 
 
 germania.authors = authors
+
+
+def a_minus_b(a: set | list, b: set | list) -> set | list:
+    result = []
+    for item in a:
+        if item in b:
+            continue
+        result.append(item)
+    if isinstance(a, set):
+        result: set = set(result)
+    return result
+
+
+utilo.a_minus_b = a_minus_b
+
+
+def union(*items: set | list):
+    """\
+    >>> union([1, 2, 3])
+    [1, 2, 3]
+    >>> union([1, 2, 3], [2, 2, 2])
+    [1, 2, 3]
+    """
+    result = []
+    for item in items:
+        for it in item:
+            result.append(it)
+    result = utilo.make_unique(result)
+    return result
+
+
+utilo.union = union
